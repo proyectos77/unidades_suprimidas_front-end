3772d15a5ae5b837b0e8b52f1602f29c182addec
Johan Montero <johan00712@gmail.com>
2025-06-01 11:58:45 -0500

    JMontero - Actualizacion de formulario y las interfaces.

--- src/app/Features/Unidades/interfaces/store-archivo-detalle-unidad.ts
+++ src/app/Features/Unidades/interfaces/store-archivo-detalle-unidad.ts
@@ -2,6 +2,8 @@ export interface StoreArchivoDetalleUnidad {
   numero_carpetas:        number;
   numero_cajas:           number;
   numero_folio:           number;
+  numero_tomos:           number | null;
+  otros:                  number | null;
   anio_registro_archivo:  number;
   id_detalle:             number;
 }
--- src/app/Features/Unidades/pages/registro-archivo-unidad/registro-archivo-unidad.component.ts
+++ src/app/Features/Unidades/pages/registro-archivo-unidad/registro-archivo-unidad.component.ts
@@ -55,6 +55,8 @@ export default class RegistroArchivoUnidadComponent {
         numero_cajas: ['', Validators.required],
         numero_carpetas: ['', Validators.required],
         numero_folio: ['', Validators.required],
+        numero_tomos: [''],
+        otros: [''],
       }));
     }
 
