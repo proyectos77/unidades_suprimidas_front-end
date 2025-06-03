c17758e102fd4aeec3738feb2f1fc770772ba115
Johan Montero <johan00712@gmail.com>
2025-06-03 17:15:21 -0500

    JMontero - Configuracion de ruta dinamica para compilacion

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,20 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  {
+    path: 'main/unidades/informacionUnidad/:id',
+    renderMode: RenderMode.Server // O simplemente omite 'renderMode' para que use SSR dinámico
+                                   // si esa es tu configuración por defecto.
+                                   // RenderMode.Server es más explícito.
+  },
+
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
+
+
+
