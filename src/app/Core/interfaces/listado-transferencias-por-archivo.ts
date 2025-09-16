30d8f9fb93de3e966bf044975c6a5e2a4767937f
JCmontero22 <johan00712@gmail.com>
2025-09-16 13:35:38 -0500

    Modificaciones en el detalle del archivo en la unidad

--- src/app/Core/interfaces/listado-transferencias-por-archivo.ts
+++ src/app/Core/interfaces/listado-transferencias-por-archivo.ts
@@ -1,5 +1,6 @@
 
 
+
 export interface ListadoTransferenciasPorArchivo {
   statusCode:     number;
   titulo:         string;
@@ -22,16 +23,36 @@ export interface DetalleTransferencia {
   id_detalle_transferencia:                  number;
   id_transferencia:                          number;
   seccion_detalle_transferencia:             string;
-  serie_detalle_transferencia:               string;
-  subserie_detalle_transferencia:            string;
+  id_serie:                                  number;
+  id_subserie:                               number;
   cantidad_cajas_detalle_transferencia:      number;
   cantidad_carpetas_detalle_transferencia:   number;
   cantidad_otros_detalle_transferencia:      number;
+  cantidad_tomos_detalle_transferencia:      number;
   cantidad_folios_detalle_transferencia:     number;
   porcentaje_detalle_transferencia:          string;
   fecha_creacion_detalle_transferencia:      Date;
   fecha_actualizacion_detalle_transferencia: Date;
   id_estado:                                 number;
+  serie:                                     Serie;
+  subserie:                                  Subserie;
+}
+
+export interface Serie {
+  id_serie:          number;
+  nombre_serie:      string;
+  codigo_serie:      number;
+  anio_inicio_serie: number;
+  anio_fin_serie:    number;
+  id_estado:         number;
+}
+
+export interface Subserie {
+  id_subserie:     number;
+  codigo_subserie: number;
+  nombre_subserie: string;
+  id_serie:        number;
+  id_estado:       number;
 }
 
 export interface InfoPagination {
